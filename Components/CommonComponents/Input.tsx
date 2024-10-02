4a9df2573cb6b42198b815d55d73c4b05aa7333c
yaswanth <aysawnth@enphaseenergy.com>
2024-10-02 18:04:59 +0530

    Added DashBoard

--- Components/CommonComponents/Input.tsx
+++ Components/CommonComponents/Input.tsx
@@ -4,7 +4,7 @@ export default function Input({props}: any) {
 
     return <>
         <div className="input-group mb-3">
-            <input type="text" className="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" placeholder={props.placeholder} id="login" value={props.value} onChange={props.onchange}/>
+            <input type="text" className="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" placeholder={props.placeholder} id="login" value={props.value} onChange={props.onchange} disabled={props.disable} style = {{height: "48px"}}/>
         </div>
     </>
 
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import Dashboard from "@/Components/DashboardComponents/Dashboard"
+
+
+
+export default function DashboardTree() {
+    return <>
+    <Dashboard />
+    </>
+}
